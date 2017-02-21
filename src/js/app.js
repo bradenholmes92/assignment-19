e74dbb390b952cb81d0130cbb2d9d7fbc1e36843
Braden Holmes <bradenholmes92@Bradens-MacBook-Pro.local>
2017-02-21 13:03:15 -0500

    initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,11 @@
-console.log('wat')
+
+import $ from 'jquery';
+console.log($)
+
+//github token : ca0d5671a67e9ce36e5ab9a141a055b66562284e
+
+var forEach = function(arr, cb){
+for(var i = 0 ; i < arr.length; i++){
+  {cb(arr[i], i, arr)}
+}
+}
